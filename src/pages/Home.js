023f40aa2982cab30251ee41be83fa6c4d94a9bc
Lydia Yang <lydiayang92@gmail.com>
2023-02-22 15:09:01 -0800

    almost got the backgruodn image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,16 @@
-import React, { useEffect } from 'react'
-import background from '../assets/landing-page-background.png'
+import React from 'react';
+import background from '../assets/landing-page-background.png';
 import './Home.css'
 
 function Home() {
-    const [windowWidth, setWindowWidth] = React.useState(window.innerWidth)
-    const imageUrl = windowWidth >= 768 ? background : background
 
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowWidth(window.innerWidth)
-        }
-        window.addEventListener('resize', handleResize)
-        return () => window.removeEventListener('resize', handleResize)
-    });
-        
+   
+
     return (
-    <div className='container'>Experience the Playful Dishes with Chick in the Box</div>
-  )
+       <div className='image-container'>
+        <img className='background-image' src={background} alt='background' />
+       </div>
+    );
 }
 
 export default Home
