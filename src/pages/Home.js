bdb7d834f820807d0b19bf444c3c5746a5780aad
Lydia Yang <lydiayang92@gmail.com>
2023-02-22 23:21:13 -0800

    adjusting media query

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import background from "../assets/landing-page.png";
 import "./Home.css";
 
 function Home() {
-  const [windowSize, setWindowSize] = useState([
-    window.innerWidth,
-    window.innerHeight,
-  ]);
-
-  useEffect(() => {
-    const handleWindowResize = () => {
-      setWindowSize([window.innerWidth, window.innerHeight]);
-    };
-
-    window.addEventListener("resize", handleWindowResize);
-
-    return () => window.removeEventListener("resize", handleWindowResize);
-  }, []);
-
   return (
     <div className="image-container">
       <img className="image-container" src={background} alt="background" />
       <div className="text-container">
         <h1>experience the playful dishes with chick in the box</h1>
-        {/* <p>Width: {windowSize[0]}</p>
-        <p>Height: {windowSize[1]}</p> */}
       </div>
     </div>
   );
