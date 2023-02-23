6045c177a1e519ac17762a7bf5c7138e1c71bb5a
Lydia Yang <lydiayang92@gmail.com>
2023-02-22 17:32:32 -0800

    added heading

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,9 @@ function Home() {
     return (
        <div className='image-container'>
         <img className='image-container'src={background} alt='background' />
+        <div className='text-container'>
+            <h1>experience the playful dishes with chick in the box</h1>
+        </div>
        </div>
     );
 }
