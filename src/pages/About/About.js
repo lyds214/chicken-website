6b5c69e5f61e7975fe28dacdc47fc853a6af0074
Lydia Yang <lydiayang92@gmail.com>
2023-02-27 23:07:11 -0800

    added font style

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,19 +1,28 @@
-import React from 'react'
-import cafe from '../../assets/about.png'
-import '../About/About.css'
+import React from "react";
+import cafe from "../../assets/about.png";
+import "../About/About.css";
 
 const About = () => {
   return (
-    <div className='img-container'>
-      <img src={cafe} alt='cafe' />
+    <div className="img-container">
+      <img src={cafe} alt="cafe" />
 
-      <div className='about-chick-container text-style'>
-        <h1>About Chick in the Box</h1>
-        <p>Chick in the Box a locally owned business that is quality driven to provide the best products to our customers.</p>
-        <p>We enjoy offering our customers with classic dishes as well as creative and playful dishes that we have conceptualized while keeping fresh and seasonal ingredients in mind.</p>
+      <div className="about-chick-container">
+        <h1 className="heading">About Chick in the Box</h1>
+        <div className="paragraph">
+          <p>
+            Chick in the Box a locally owned business that is quality driven to
+            provide the best products to our customers.
+          </p>
+          <p>
+            We enjoy offering our customers with classic dishes as well as
+            creative and playful dishes that we have conceptualized while
+            keeping fresh and seasonal ingredients in mind.
+          </p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
