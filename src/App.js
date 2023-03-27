311272c75afe3627ea4ee52228a9e1df0a94f955
Lydia Yang <lydiayang92@gmail.com>
2023-03-26 18:55:11 -0700

    starting over

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
-import Specialties from "./pages/Our Specialties/Specialties";
 
 function App() {
   return (
     <div className="App">
-      <Specialties />
+      <Home />
     </div>
   );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,31 +1,7 @@
-import React, {useState, useEffect} from 'react';
-import background from '../../assets/background.png';
-import "../Home/Home.css";
+import React from "react";
 
 function Home() {
-
-  const [windowSize, setWindowSize] = useState([window.innerWidth, window.innerHeight]);
-
-  useEffect(() => {
-      const handleWindowResize = () => {
-          setWindowSize([window.innerWidth, window.innerHeight]);
-      }
-
-      window.addEventListener('resize', handleWindowResize);
-
-      return () => window.removeEventListener('resize', handleWindowResize);
-  }, []);
-
-  return (
-    <div className="image-container text-heading">
-      <img className="image-container" src={background} alt="background" />
-      <div className="text-container">
-        <h1>EXPERIENCE THE PLAYFUL DISHES WITH CHICK IN THE BOX</h1>
-         {/* <p>Width: {windowSize[0]}</p>
-            <p>Height: {windowSize[1]}</p> */}
-      </div>
-    </div>
-  );
+  return <div>Hello</div>;
 }
 
 export default Home;
