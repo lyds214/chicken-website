25225592f487445b3934b401deaed73950ea0f41
Lydia Yang <lydiayang92@gmail.com>
2023-03-28 13:16:31 -0700

    added background img

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "./fonts/Poppins/Poppins-Regular.ttf";
+import "./assets/fonts/Poppins-Regular.ttf";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
+import background from "../../assets/img/landing.png";
+import "./Home.css";
 
 function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <img src={background} alt="background" className="background-img" />
+    </div>
+  );
 }
 
 export default Home;
