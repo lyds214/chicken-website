98387d07db26d0bb202eea07f77f1ead07f470d7
Lydia Yang <lydiayang92@gmail.com>
2023-03-28 14:09:16 -0700

    added About files

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,7 +1,13 @@
 import React from "react";
+import cafe from "../../assets/img/cafe.png";
+import "./About.css";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="cafe-image">
+      <img src={cafe} alt="cafe" />
+    </div>
+  );
 };
 
 export default About;
