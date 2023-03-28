77725e22e47d26448af4c75f81295c106ba860a2
Lydia Yang <lydiayang92@gmail.com>
2023-03-28 13:19:07 -0700

    added title

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,6 +6,11 @@ function Home() {
   return (
     <div>
       <img src={background} alt="background" className="background-img" />
+      <div>
+        <h1 className="title">
+          EXPERIENCE THE PLAYFUL DISHES WITH CHICK IN THE BOX
+        </h1>
+      </div>
     </div>
   );
 }
