18524ffbb2f41d2abbe94cbe40b5d1636b9e4e36
Lydia Yang <lydiayang92@gmail.com>
2023-03-28 17:38:05 -0700

    finished about page layout

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,8 +4,24 @@ import "./About.css";
 
 const About = () => {
   return (
-    <div className="cafe-image">
-      <img src={cafe} alt="cafe" />
+    <div className="container">
+      <div className="image-container">
+        <img src={cafe} alt="cafe" />
+      </div>
+      <div className="about-text">
+        <h1 style={{ fontFamily: "Poppins-Semibold" }}>
+          About Chick in the Box
+        </h1>
+        <p style={{ fontFamily: "Poppins" }}>
+          Chick in the Box is a locally owned business that is quality driven to
+          provide the best products to our customers.
+        </p>
+        <p style={{ fontFamily: "Poppins" }}>
+          We enjoy offering our customers with classic dishes as well as
+          creative and playful dishes that we have conceptualized while keeping
+          fresh and seasonal ingredients in mind.
+        </p>
+      </div>
     </div>
   );
 };
