56db3bb0f9d52e161cbdbbedd41cff5e92c67dd7
Lydia Yang <lydiayang92@gmail.com>
2023-03-29 11:21:50 -0700

    food component setup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Specialties from "./pages/Specialties/Specialties";
+import Food from "./components/Food";
 
 function App() {
   return (
     <div className="App">
-      <Specialties />
+      <Food />
     </div>
   );
 }
