926556b63d00d9dce0b145639e99a47f8a276912
Lydia Yang <lydiayang92@gmail.com>
2023-03-29 11:01:02 -0700

    installed mui and added specialties files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Specialties from "./pages/Specialties/Specialties";
 
 function App() {
   return (
     <div className="App">
-      <About />
+      <Specialties />
     </div>
   );
 }
---
+++ src/pages/Specialties/Specialties.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+
+const Specialties = () => {
+  return <div>Specialties</div>;
+};
+
+export default Specialties;
