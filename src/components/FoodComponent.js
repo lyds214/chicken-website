9e66ca8288d71969328cc7174571c0cfab14e102
Lydia Yang <lydiayang92@gmail.com>
2023-03-29 12:16:56 -0700

    added hover effect

--- src/components/FoodComponent.js
+++ src/components/FoodComponent.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import chicken from "../assets/img/chicken.png";
 
 const FoodComponent = () => {
-  return <div>Food</div>;
+  const [isShown, setIsShown] = useState(false);
+
+  return (
+    <div>
+      <img src={chicken} alt="chicken" onMouseEnter={() => setIsShown(true)} />
+      {isShown && <div>Chicken</div>}
+    </div>
+  );
 };
 
 export default FoodComponent;
