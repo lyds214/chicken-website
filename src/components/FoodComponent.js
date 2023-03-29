0dd988596839330c181ed139dcbadbe0849573ca
Lydia Yang <lydiayang92@gmail.com>
2023-03-29 13:03:13 -0700

    added onMouseLeave effect

--- src/components/FoodComponent.js
+++ src/components/FoodComponent.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import chicken from "../assets/img/chicken.png";
+import "./FoodComponent.css";
 
 const FoodComponent = () => {
   const [isShown, setIsShown] = useState(false);
 
   return (
-    <div>
-      <img src={chicken} alt="chicken" onMouseEnter={() => setIsShown(true)} />
-      {isShown && <div>Chicken</div>}
+    <div style={{ width: "29.063em", height: "67.5em" }}>
+      <img
+        src={chicken}
+        alt="chicken"
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}
+      />
+      {isShown && <div>Hello</div>}
     </div>
   );
 };
