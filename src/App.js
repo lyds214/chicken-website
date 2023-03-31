db6636b5114df4b1e44d4bc6f578b6d0a596d601
Lydia Yang <lydiayang92@gmail.com>
2023-03-31 11:23:12 -0700

    added location files

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Specialties from "./pages/Specialties/Specialties";
 import FoodComponent from "./components/FoodComponent";
+import Location from "./pages/Location/Location";
 
 function App() {
   return (
     <div className="App">
-      <FoodComponent />
+      <Location />
     </div>
   );
 }
