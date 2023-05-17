eae5321dcf26a1b77b662b107ec64cfe083a81e1
Lydia Yang <64216620+lyds214@users.noreply.github.com>
2023-05-17 11:58:21 -0700

    added left padding

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,21 +1,28 @@
 import React from "react";
 import cafe from "../../assets/img/cafe.png";
-import "./About.css";
 
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper'
+import Box from '@mui/material/Box'
 
 const About = () => {
 
   return (
-   <Grid container spacing={8} alignItems="center" justify='center'>
+    <Box   
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+      sx={{ml: 8}}
+    >
+    <Grid container spacing={8} justify="center" alignItems="center">
         <Grid item>
           <img src={cafe} alt="cafe" />
         </Grid>
-        <Grid item xs={12} sm container alignItems="center" justify='center'>
-          <Grid item xs container spacing={2} >
+        <Grid item xs={12} sm container>
+          <Grid item xs container>
             <Grid item xs >
               <Typography variant="h2">About Chick In The Box</Typography>
               <Grid item xs>
@@ -26,20 +33,8 @@ const About = () => {
           </Grid>
         </Grid>
       </Grid>
-    // <Grid container spacing={2}>
-    //   <Grid item>
-    //     <img src={cafe} alt="cafe" />
-    //   </Grid>
-    //   <Grid item container>
-    //     <Typography variant="h2">About Chick In The Box</Typography>
-    //   </Grid>
-    //   <Grid item>
-    //     <Typography variant="p">Chick in the Box is a locally owned business that is quality driven to provide the best products to our customers.</Typography>
-    //   </Grid>
-    //   <Grid item>
-    //     <Typography variant="p">We enjoy offering our customers with classic dishes as well as creative and playful dishes that we have conceptualized while keeping fresh and seasonal ingredients in mind.</Typography>
-    //   </Grid>
-    // </Grid>
+    </Box>
+
 
   );
 };
