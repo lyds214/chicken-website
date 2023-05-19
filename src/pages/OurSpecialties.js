1b40b6b21a0531afcb8dbe1f96492c4b5a525d14
Lydia Yang <64216620+lyds214@users.noreply.github.com>
2023-05-19 11:41:08 -0700

    deleted Food component

--- src/pages/OurSpecialties.js
+++ src/pages/OurSpecialties.js
@@ -1,10 +1,53 @@
 import React from 'react'
-import Food from '../components/Food'
-import '../components/data'
+
+import bibimbap from '../assets/img/bibimbap.png'
+import porkcutlet from '../assets/img/cutlet.png'
+import chicken from '../assets/img/chicken.png'
+import beefbowl from '../assets/img/beef-bowl.png'
+
+import Box from '@mui/material/Box'
+import { Typography, Button, Grid, Paper } from '@mui/material'
 
 const OurSpecialties = () => {
+
+    const foods = [
+      {
+          id: 1,
+          name: "Bibimbap",
+          price: "$14.99",
+          img: bibimbap
+      },
+      {
+          id: 2,
+          name: "Fried Chicken",
+          price: "$7.99",
+          img: chicken
+      },
+      {
+          id: 3,
+          name: "Beef Bowl",
+          price: "$8.99",
+          img: beefbowl
+      },
+      {
+          id: 4,
+          name: "Pork Cutlet",
+          price: "$16.99",
+          img: porkcutlet
+      }
+  ];
+
+  const styles = {
+    paperContainer: {
+        width: 465,
+        height: 1080,
+    }
+  }
+
   return (
-    <div><Food name='chicken' price='14.99'/></div>
+    <Paper style={styles.paperContainer}>
+   Hello
+ </Paper>
   )
 }
 
