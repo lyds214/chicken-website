65aaf5b2b1ec2767cbc69ff241e6d4526f9ce264
Lydia Yang <64216620+lyds214@users.noreply.github.com>
2023-05-22 12:47:52 -0700

    ourspecialties layout done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
-      <Footer />
+      <OurSpecialties />
     </div>
   );
 }
--- src/pages/OurSpecialties.js
+++ src/pages/OurSpecialties.js
@@ -1,15 +1,137 @@
-import React from 'react'
-import Food from '../components/Food'
-import {data} from '../components/data'
+import React from "react";
+import beefbowl from "../assets/img/beef-bowl.png";
+import bibimbap from "../assets/img/bibimbap.png";
+import chicken from "../assets/img/chicken.png";
+import cutlet from "../assets/img/cutlet.png";
+
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { Card } from "@mui/material";
+
+const images = [
+  {
+    url: beefbowl,
+    title: "Beef Bowl",
+    width: "25%",
+  },
+  {
+    url: bibimbap,
+    title: "Bibimbap",
+    width: "25%",
+  },
+
+  {
+    url: chicken,
+    title: "Chicken",
+    width: "25%",
+  },
+  {
+    url: cutlet,
+    title: "Cutlet",
+    width: "25%",
+  },
+];
+
+const ImageButton = styled(Card)(({ theme }) => ({
+  position: "relative",
+  height: 929,
+  [theme.breakpoints.down("sm")]: {
+    width: "100% !important", // Overrides inline-style
+    height: 100,
+  },
+  "&:hover, &.Mui-focusVisible": {
+    zIndex: 1,
+    "& .MuiImageBackdrop-root": {
+      opacity: 0.15,
+    },
+    "& .MuiImageMarked-root": {
+      opacity: 0,
+    },
+    "& .MuiTypography-root": {
+      border: "4px solid currentColor",
+    },
+  },
+}));
+
+const ImageSrc = styled("span")({
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundSize: "cover",
+  backgroundPosition: "center 40%",
+});
+
+const Image = styled("span")(({ theme }) => ({
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: theme.palette.common.white,
+}));
+
+const ImageBackdrop = styled("span")(({ theme }) => ({
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundColor: theme.palette.common.black,
+  opacity: 0.4,
+  transition: theme.transitions.create("opacity"),
+}));
+
+const ImageMarked = styled("span")(({ theme }) => ({
+  height: 3,
+  width: 100,
+  backgroundColor: theme.palette.common.white,
+  position: "absolute",
+  bottom: -2,
+  left: "calc(50% - 45px)",
+  transition: theme.transitions.create("opacity"),
+}));
 
 const OurSpecialties = () => {
   return (
-    <>
-      {data.map(data => {
-          <Food key={data.id} name={data.name} price={data.price}></Food>
-      })}
-    </>
-  )
-}
+    <Box
+      sx={{ display: "flex", flexWrap: "wrap", minWidth: 300, width: "100%" }}
+    >
+      {images.map((image) => (
+        <ImageButton
+          focusRipple
+          key={image.title}
+          style={{
+            width: image.width,
+          }}
+        >
+          <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+          <ImageBackdrop className="MuiImageBackdrop-root" />
+          <Image>
+            <Typography
+              component="span"
+              variant="h3"
+              color="inherit"
+              sx={{
+                position: "relative",
+                p: 4,
+                pt: 2,
+                pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+              }}
+            >
+              {image.title}
+              <ImageMarked className="MuiImageMarked-root" />
+            </Typography>
+          </Image>
+        </ImageButton>
+      ))}
+    </Box>
+  );
+};
 
-export default OurSpecialties
+export default OurSpecialties;
