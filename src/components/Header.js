3c03447161b8496a0d78447704a3c2ce798b18a7
Lydia Yang <64216620+lyds214@users.noreply.github.com>
2023-05-23 12:55:51 -0700

    fixed layout of the entire website

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,54 +19,30 @@ const ColorButton = styled(Button)(({ theme }) => ({
   padding: 10,
 }));
 
-function HideOnScroll(props) {
-  const { children, window } = props;
-
-  const trigger = useScrollTrigger({
-    target: window ? window() : undefined,
-  });
-
-  return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  );
-}
-
-HideOnScroll.propTypes = {
-  children: PropTypes.element.isRequired,
-  window: PropTypes.func,
-};
-
 const Header = (props) => {
   return (
-    <React.Fragment>
-      <HideOnScroll {...props}>
-        <AppBar
-          position="static"
-          style={{
-            backgroundColor: "black",
-          }}
-        >
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              CHICK IN THE BOX
-            </Typography>
-            <Button variant="text" sx={{ color: "white" }}>
-              MENU
-            </Button>
-            <Button variant="text" sx={{ color: "white" }}>
-              ABOUT US
-            </Button>
-            <Button variant="text" sx={{ color: "white" }}>
-              SPECIALTY
-            </Button>
-            <ColorButton color="inherit">ORDER ONLINE</ColorButton>
-          </Toolbar>
-        </AppBar>
-      </HideOnScroll>
-      <Toolbar />
-    </React.Fragment>
+    <AppBar
+      position="static"
+      style={{
+        backgroundColor: "black",
+      }}
+    >
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          CHICK IN THE BOX
+        </Typography>
+        <Button variant="text" sx={{ color: "white" }}>
+          MENU
+        </Button>
+        <Button variant="text" sx={{ color: "white" }}>
+          ABOUT US
+        </Button>
+        <Button variant="text" sx={{ color: "white" }}>
+          SPECIALTY
+        </Button>
+        <ColorButton color="inherit">ORDER ONLINE</ColorButton>
+      </Toolbar>
+    </AppBar>
   );
 };
 
