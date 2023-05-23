43e3ed1ef4952eb38676d02f9788d0d31cfe493b
Lydia Yang <64216620+lyds214@users.noreply.github.com>
2023-05-23 12:33:36 -0700

    changed footer color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import { Box, Typography } from "@mui/material";
 const Footer = () => {
   return (
     <div>
-      <Box sx={{ backgroundColor: "primary.light", height: 56 }}>
-        <Typography align="center" sx={{ paddingTop: 2 }}>
+      <Box sx={{ backgroundColor: "red", height: 56 }}>
+        <Typography align="center" sx={{ paddingTop: 2, color: "white" }}>
           2022 All Rights Reserved
         </Typography>
       </Box>
