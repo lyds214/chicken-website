418fbe1a84642831070369daebd83c677560cbf4
Lydia Yang <64216620+lyds214@users.noreply.github.com>
2023-05-24 14:06:24 -0700

    refactored some code

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,40 +1,19 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
-import styled from "styled-components";
 import "./styles/Calendar.css";
 
 const Menu = () => {
-  const CalendarContainer = styled.div`
-    /* ~~~ container styles ~~~ */
-    /* ~~~ navigation styles ~~~ */
-    /* ~~~ label styles ~~~ */
-    /* ... */
-
-    /* ~~~ button styles ~~~ */
-    button {
-      border: 0;
-      border-radius: 3px;
-      color: white;
-    }
-
-    .react-calendar__month-view__days__day--neighboringMonth {
-      opacity: 0.5;
-      color: #818589;
-    }
-  `;
   return (
     <div>
-      <CalendarContainer>
-        <Calendar
-          prevLabel={null}
-          nextLabel={null}
-          next2Label={null}
-          prev2Label={null}
-          calendarType="US"
-          maxDetail="month"
-          style={{ color: "white" }}
-        />
-      </CalendarContainer>
+      <Calendar
+        prevLabel={null}
+        nextLabel={null}
+        next2Label={null}
+        prev2Label={null}
+        calendarType="US"
+        maxDetail="month"
+        style={{ color: "white" }}
+      />
     </div>
   );
 };
