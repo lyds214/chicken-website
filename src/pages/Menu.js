7e6cfec68d793d8624f45836a148bc27b19da5c9
Lydia Yang <64216620+lyds214@users.noreply.github.com>
2023-05-24 14:47:14 -0700

    added heading

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,10 +1,25 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "./styles/Calendar.css";
+import { Box, Typography } from "@mui/material";
 
 const Menu = () => {
   return (
     <div>
+      <Box
+        sx={{
+          backgroundColor: "black",
+          paddingTop: "3em",
+          paddingBottom: "1.5em",
+        }}
+      >
+        <Typography
+          variant="h4"
+          style={{ color: "white", fontWeight: "bold", marginLeft: "3em" }}
+        >
+          This month's special menu
+        </Typography>
+      </Box>
       <Calendar
         prevLabel={null}
         nextLabel={null}
@@ -12,7 +27,6 @@ const Menu = () => {
         prev2Label={null}
         calendarType="US"
         maxDetail="month"
-        // showNavigation={false}
         style={{ color: "white" }}
       />
     </div>
